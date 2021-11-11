e9fddb159338005e097e1b6a8813253f78c0e605
Maria Budzanowska <budzanowska.maria@gmail.com>
2021-11-11 18:41:53 +0100

    calculating file paths

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,8 +1,12 @@
-import { Command } from "commander";
+import path from "path";
+import { Command, option } from "commander";
+import { serve } from "local-api";
 
 export const serveCommand = new Command()
-  .command('serve')
-  .description('Open a file for editing')
-  .action(() => {
-    console.log("Getting ready to serve a file")
+  .command("serve [filename] ")
+  .description("Open a file for editing")
+  .option("-p, --port <number>", "port to run server on", "4005")
+  .action((filename = "notebook.js", options: { port: string }) => {
+    const dir = path.join(process.cwd(), path.dirname(filename));
+    serve(parseInt(options.port), path.basename(filename), dir);
   });
