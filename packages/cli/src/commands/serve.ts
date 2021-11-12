6fb642544fb3f9e6069e4acf00d768b49cd8cdd6
Maria Budzanowska <budzanowska.maria@gmail.com>
2021-11-12 15:37:33 +0100

    add organization name and update imports

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,6 +1,6 @@
 import path from "path";
-import { Command, option } from "commander";
-import { serve } from "local-api";
+import { Command } from "commander";
+import { serve } from "@mb-jsnote/local-api";
 
 const isProduction = process.env.NODE_ENV === "production";
 
