b2d5e89af8887aa7a5d714574ccaddefaf206889
Maria Budzanowska <budzanowska.maria@gmail.com>
2021-11-12 15:57:07 +0100

    update dependencies and devDependencies

--- packages/cli/dist/notebook.js
+++ packages/cli/dist/notebook.js
@@ -1,1 +1,1 @@
-[{"content":"const dupa  = 1;\r\nconsole.log(dupa);\r\ndsadsaadasdsaadadsad","type":"code","id":"xqvx3"}]
+[{"content":"const dupa  = 1;\r\nconsole.log(dupa);","type":"code","id":"xqvx3"}]
